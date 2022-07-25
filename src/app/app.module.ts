2fa29d9bc1293bf554f40945c3db6270e89ed168
Luiz Abbott <luiz_abbott@epam.com>
2022-07-25 17:01:15 -0300

    Initial project configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
+import { ListingsPageComponent } from './listings-page/listings-page.component';
+import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
+import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
+import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListingsPageComponent,
+    ListingDetailPageComponent,
+    ContactPageComponent,
+    MyListingsPageComponent,
+    NewListingPageComponent,
+    EditListingPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
