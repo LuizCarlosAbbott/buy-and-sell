88a3f1f2edb79a18e01ead34e832212447178d21
Luiz Abbott <luiz_abbott@epam.com>
2022-07-25 21:05:59 -0300

    feat: listing-detail-page

--- src/app/listing-detail-page/listing-detail-page.component.ts
+++ src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-listing-detail-page',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listing-detail-page.component.css']
 })
 export class ListingDetailPageComponent implements OnInit {
+  listing: Listing | undefined;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id);
   }
 
 }
