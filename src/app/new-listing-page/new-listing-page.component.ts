294c3679b28807244832ecf4c31e16c78e0548f0
Luiz Abbott <luiz_abbott@epam.com>
2022-07-25 21:29:17 -0300

    feat: new listing page

--- src/app/new-listing-page/new-listing-page.component.ts
+++ src/app/new-listing-page/new-listing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-listing-page',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-listing-page.component.css']
 })
 export class NewListingPageComponent implements OnInit {
+  public name: string = '';
+  public description: string = '';
+  public price: string = '';
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+
   }
 
+  public onSubmit(): void {
+    alert('Creating a new listing...');
+    this.router.navigateByUrl('/my-listings');
+  }
 }
